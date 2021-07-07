df7f265f9c9a062d20ebf30e4b32cea39cab7494
Leighton Chien <leightonchien@gmail.com>
2021-07-07 00:10:53 -0700

    Add dark background with gray text using tailwind css

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Gallery from "./components/Gallery";
 
 export default function App() {
   return (
-    <main>
+    <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
       <About />
       <Projects />
