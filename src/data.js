eba4caa8e3be22269346c31d29b10fb078f8db3d
Leighton Chien <leightonchien@gmail.com>
2021-07-08 22:07:09 -0700

    update testimonial

--- src/data.js
+++ src/data.js
@@ -21,17 +21,17 @@ export const projects = [
 export const testimonials = [
   {
     quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+      "Leighton is a creative problem solver. His works are exceptional.",
     image: "https://randomuser.me/api/portraits/men/1.jpg",
-    name: "Jesse Hicks",
-    company: "Zoozle",
+    name: "Milton B.",
+    company: "Sixtwo",
   },
   {
     quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+      "Leighton has an eye for visual design, it was a pleasure working with him.",
     image: "https://randomuser.me/api/portraits/men/94.jpg",
-    name: "Ruben Alvarez",
-    company: "Dooble.io",
+    name: "Kurt C.",
+    company: "Fig",
   },
 ];
 
